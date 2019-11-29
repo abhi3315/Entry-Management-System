17a9eedd5c31c68ad86b47c8abeb326b2b5361f6
Abhishek Kumar Sharma <abhi.bp143cp@gmail.com>
2019-11-29 21:17:28 +0530

    Added IMPORTANT.txt

--- sendsmshost.py
+++ sendsmshost.py
@@ -2,14 +2,14 @@ def send_sms_host(mobile,a,b,c,d):
     from twilio.rest import Client
 
 
-    account_sid = 'AC21bf13a2b77a7f256f82dfc54f712fa7'
-    auth_token = '7772d41bbcbf302598e10d9cd4817bdd'
+    account_sid = 'paste ACCOUNT SID here'
+    auth_token = 'paste AUTH TOKEN here'
     client = Client(account_sid, auth_token)
     msg="Subject: Visitor-Details \n\n Visitor Name: %s \n Checkin Time: %s \n Visitor Mobile: %s \n Visitor Email: %s"%(a,b,c,d)
 
     message = client.messages.create(
              body=msg,
-             from_='+19515470170',
+             from_='paste NUMBER here',
              to='+91'+mobile
          )
 
